bd734e0b4a01f8b5ce4d6b34d62b816d3f7e3921
Alexis <alexis@cvofhope.com>
2019-11-09 10:40:12 +1100

    run basic http server

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -1,2 +1,7 @@
-# Hello Nim!
-echo "Hello, World!"
+import asynchttpserver, asyncdispatch
+
+var server = newAsyncHttpServer()
+proc cb(req: Request) {.async.} =
+  await req.respond(Http200, "Nim says Hello World!")
+
+waitFor server.serve(Port(8080), cb)
