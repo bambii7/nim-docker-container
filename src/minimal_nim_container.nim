1193ea6100a594bba228d7c9a7b124dbf93374f3
Alexis <alexis@cvofhope.com>
2019-11-09 11:23:51 +1100

    read and return env var

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -1,7 +1,10 @@
 import asynchttpserver, asyncdispatch
+import os
 
 var server = newAsyncHttpServer()
 proc cb(req: Request) {.async.} =
-  await req.respond(Http200, "Nim says Hello World!")
+  let envMessage = getEnv("MESSAGE", "no message")
+  let response = "Nim says Hello World!" & envMessage
+  await req.respond(Http200, response)
 
 waitFor server.serve(Port(8080), cb)
