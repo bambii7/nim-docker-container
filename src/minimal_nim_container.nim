2f334b1da84f91890036ece020bb4fe81438abc2
Alexis <alexis@cvofhope.com>
2019-11-09 11:49:27 +1100

    connect and query postgres

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -1,10 +1,17 @@
 import asynchttpserver, asyncdispatch
 import os
+import db_postgres
 
 var server = newAsyncHttpServer()
+
 proc cb(req: Request) {.async.} =
+  let db = open("localhost", "", "", "awesome_sauce")
+  let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
+  let dbResult = db.getValue(query)
+
   let envMessage = getEnv("MESSAGE", "no message")
-  let response = "Nim says Hello World!" & envMessage
+
+  let response = "Nim says Hello World! \n" & envMessage & "\n" & $dbResult
   await req.respond(Http200, response)
 
 waitFor server.serve(Port(8080), cb)
