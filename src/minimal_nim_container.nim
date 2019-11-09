fbee60e999c2ab607fb43d8afaccfaa8120b82f7
Alexis <alexis@cvofhope.com>
2019-11-09 11:55:05 +1100

    use full name for callback proc

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -4,7 +4,7 @@ import db_postgres
 
 var server = newAsyncHttpServer()
 
-proc cb(req: Request) {.async.} =
+proc callback(req: Request) {.async.} =
   let db = open("localhost", "", "", "awesome_sauce")
   let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
   let dbResult = db.getValue(query)
@@ -14,4 +14,4 @@ proc cb(req: Request) {.async.} =
   let response = "Nim says Hello World! \n" & envMessage & "\n" & $dbResult
   await req.respond(Http200, response)
 
-waitFor server.serve(Port(8080), cb)
+waitFor server.serve(Port(8080), callback)
