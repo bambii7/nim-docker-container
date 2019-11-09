88779d64b1c62e6a69bf369873a83633e6313e27
Alexis <alexis@cvofhope.com>
2019-11-09 19:13:08 +1100

    refactor query to proc with guard

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -4,14 +4,19 @@ import db_postgres
 
 var server = newAsyncHttpServer()
 
-proc callback(req: Request) {.async.} =
-  let db = open("localhost", "", "", "awesome_sauce")
-  let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
-  let dbResult = db.getValue(query)
+proc queryDB():string =
+  try:
+    let db = open("", "", "", "dbname=awesome_sauce host=172.17.0.0/16 port=5432")
+    let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
+    result = db.getValue(query)
+  except:
+    result = "No DB Found"
 
+proc callback(req: Request) {.async.} =
+  let dbResult = queryDB()
   let envMessage = getEnv("MESSAGE", "no message")
 
   let response = "Nim says Hello World! \n" & envMessage & "\n" & $dbResult
   await req.respond(Http200, response)
 
-waitFor server.serve(Port(8080), callback)
+waitFor server.serve(Port(80), callback)
