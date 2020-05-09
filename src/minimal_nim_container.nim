a6d1cb878e7085cee929dacb18a1f346e21c8b88
Alexis H <alexis@cvofhope.com>
2020-05-09 12:22:23 +1000

    simplify server

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -4,19 +4,19 @@ import db_postgres
 
 var server = newAsyncHttpServer()
 
-proc queryDB():string =
-  try:
-    let db = open("", "", "", "dbname=POSTGRES host=172.17.0.0/16 port=5432")
-    let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
-    result = db.getValue(query)
-  except:
-    result = "No DB Found"
+# proc queryDB():string =
+#   try:
+#     let db = open("", "", "", "dbname=POSTGRES host=172.17.0.0/16 port=5432")
+#     let query = db.prepare("Get PG Version", sql"SELECT version();", 0)
+#     result = db.getValue(query)
+#   except:
+#     result = "No DB Found"
 
 proc callback(req: Request) {.async.} =
-  let dbResult = queryDB()
-  let envMessage = getEnv("MESSAGE", "no message")
+  # let dbResult = queryDB()
+  # let envMessage = getEnv("MESSAGE", "no message")
 
-  let response = "Nim says Hello Worldz! \n" & envMessage & "\n" & $dbResult
+  let response = "Nim says Hello Worldzzz! \n"
   await req.respond(Http200, response)
 
 waitFor server.serve(Port(80), callback)
