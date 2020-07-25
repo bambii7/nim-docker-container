4beb5f2d1432b23361354486b17e028f6daef110
Alexis H <alexis@cvofhope.com>
2020-07-25 12:43:44 +1000

    update output

--- src/minimal_nim_container.nim
+++ src/minimal_nim_container.nim
@@ -3,6 +3,6 @@ import asynchttpserver, asyncdispatch
 let server = newAsyncHttpServer()
 
 proc callback(req: Request) {.async.} =
-  await req.respond(Http200, "Nim says Hello Worldzzz!")
+  await req.respond(Http200, "Nim says Hello World!")
 
 waitFor server.serve(Port(80), callback)
